0839ccc2f64236d86b62d3f5ca19cb2e0026ab3a
Alejandro Calixto Villavicencio <al221811773@gmail.com>
2020-03-15 12:10:49 -0600

    testeo_2 correccion databaseseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,12 @@
 <?php
 
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Comentary;
+use App\Restaurant;
+use App\Rol;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +17,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      DB::statement('SET FOREIGN_KEY_CHECKS = 0');  //Desactiva llaves foraneas
+
+      Comentary::truncate();
+      Restaurant::truncate();
+      Rol::truncate();
+      User::truncate();
+
+      Comentary::flushEventListeners();
+      Restaurant::flushEventListeners();
+      Rol::flushEventListeners();
+      User::flushEventListeners();
+
+      $cantidadComentary = 10;
+      $cantidadRestaurant = 10;
+      $cantidadRol = 10;
+      $cantidadUser = 10;
+
+      factory(Comentary::class, $cantidadComentary)->create();
+      factory(Restaurant::class, $cantidadRestaurant)->create();
+      factory(Rol::class, $cantidadRol)->create();
+      factory(User::class, $cantidadUser)->create();
+
     }
 }
